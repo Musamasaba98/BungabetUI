bd2212b9121cf42325ba44e23a3a6f228ac454d9
Masaba Musa <masamata98@gmail.com>
2020-10-12 12:14:32 -0700

    a bit of responsiveness

---
+++ dist/script.js
@@ -0,0 +1,8 @@
+const menuIcon = document.querySelector('.menu-icon');
+
+function toggleMenuIcon() {
+  menuIcon.classList.toggle('active')
+}
+
+menuIcon.addEventListener('click', toggleMenuIcon);
+
